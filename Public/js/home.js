bcf1d2e789d58a7503317adcb8bf9462858cad82
kvinzinnn <kevin.2006.rf@gmail.com>
2025-12-18 15:55:17 -0300

    Atualização geral e adição do JS

---
+++ Public/js/home.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper.mySwiper', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    }
+});
