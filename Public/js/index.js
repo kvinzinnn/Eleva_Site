4881229ec4f1513a2eab4ee0d916c6dbf188e0d8
kvinzinnn <kevin.2006.rf@gmail.com>
2025-12-22 15:54:52 -0300

    Nova Atualização

--- Public/js/index.js
+++ Public/js/index.js
@@ -18,3 +18,25 @@ const swiper = new Swiper('.swiper.mySwiper', {
         crossFade: true,
     }
 });
+
+const header = document.getElementById('cabeça');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 200) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    });
+
+const logo = document.querySelector('.container-logo');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        header.classList.add('scrolled');
+        logo.classList.add('troca');
+    } else {
+        header.classList.remove('scrolled');
+        logo.classList.remove('troca');
+    }
+});
